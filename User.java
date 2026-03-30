52643c6ebae7863e4bcc870ea70e407b037ea530
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-03-30 07:40:19 +0700

    Test

--- User.java
+++ User.java
@@ -2,7 +2,7 @@ public class User {
     static void main(String[] args) {
         System.out.println("Bai Tap Lon");
         System.out.println("hello");
-        System.out.println("Thinh vanduc");
+        System.out.println("Thinh van duc");
         System.out.println("1234");
     }
 }
