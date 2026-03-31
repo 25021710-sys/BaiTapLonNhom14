16da8bc62859e36d66a8ef390408737916b3ee29
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-03-31 22:54:47 +0700

    User

--- User.java
+++ User.java
@@ -1,5 +1,12 @@
-public class User {
-    static void main(String[] args) {
-
-    }
+abstract class User {
+    protected String userId;
+    protected String username;
+    protected String passwordHash;
+    protected String fullName;
+    protected String email;
+    public String getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getPasswordHash() { return passwordHash; }
+    public String getFullName() { return fullName; }
+    public String getEmail() { return email; }
 }
