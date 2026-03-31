5528d7f3426719d963302c1065a56d0168e79e56
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-03-31 23:10:24 +0700

    User

--- User/src/User.java
+++ User/src/User.java
@@ -1,12 +1,23 @@
-abstract class User {
+abstract class User{
     protected String userId;
     protected String username;
     protected String passwordHash;
     protected String fullName;
     protected String email;
+    protected String description;
+
+    public void setUserId(String userId){this.userId = userId;}
+    public void setUsername(String username){this.username = username;}
+    public void setPasswordHash(String passwordHash){this.passwordHash = passwordHash;}
+    public void setFullName(String fullName){this.fullName = fullName;}
+    public void setEmail(String email){this.email = email;}
+    public void setDescription(String description){this.description = description;}
+
     public String getUserId() { return userId; }
     public String getUsername() { return username; }
     public String getPasswordHash() { return passwordHash; }
     public String getFullName() { return fullName; }
     public String getEmail() { return email; }
+    public String getDescription() { return description; }
+
 }
