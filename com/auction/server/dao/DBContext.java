81f897da6bbaa38273892cd85fe0b7cc9e90b824
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-04-15 14:01:15 +0700

    User

--- com/auction/server/dao/DBContext.java
+++ com/auction/server/dao/DBContext.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class DBContext {
     public static Connection getConnection() throws Exception {
-        String url = "jdbc:mysql://metro.proxy.rlwy.net:25032/railway";
-        String user = "root";
-        String password = "clnqbcMyhShsstuEyhbjpEGwANVcpCbq";
+        String url = "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/railway";
+        String user = "2da7P72yRjHDRu2.root";
+        String password = "dwxofeHifpCuB53s";
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(url, user, password);
     }
