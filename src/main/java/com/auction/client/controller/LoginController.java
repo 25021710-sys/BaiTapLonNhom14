4946f83adf47915d33249d7afee4a8d880c7e708
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-04-19 16:42:47 +0700

    User

--- src/main/java/com/auction/client/controller/LoginController.java
+++ src/main/java/com/auction/client/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.auction.client.controller;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
     @FXML
     private TextField emailField;
@@ -23,7 +27,20 @@ public class LoginController {
         }
     }
     @FXML
-    public void goToRegister(){
-        System.out.println("Go to Register");
+    public void goToRegister(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/auction/client/view/register.fxml")
+            );
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("Sign Up");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/auction/client/controller/RegisterController.java
+++ src/main/java/com/auction/client/controller/RegisterController.java
@@ -1,4 +1,54 @@
 package com.auction.client.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RegisterController {
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    public void handleSignup(){
+        String user = usernameField.getText();
+        String email = emailField.getText();
+        String pass = passwordField.getText();
+        // Validation
+        if (user.isEmpty() || pass.isEmpty() || email.isEmpty()) {
+            errorLabel.setText("Vui lòng nhập đầy đủ thông tin");
+            errorLabel.setVisible(true);
+            return;
+        }
+    }
+    @FXML
+    public void goToLogin(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/auction/client/view/register.fxml")
+            );
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("Login");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
