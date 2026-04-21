5bc7daf92412434fc0fe7d61c6ab78da38a76fd1
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-04-21 13:39:58 +0700

    User

--- src/main/java/com/auction/client/controller/ProfileContentController.java
+++ src/main/java/com/auction/client/controller/ProfileContentController.java
@@ -1,5 +1,15 @@
 package com.auction.client.controller;
 
-public class ProfileContentController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class ProfileContentController {
+    @FXML
+    private Label displayLocation;
+    @FXML
+    private Label displayEmail;
+    @FXML
+    private Label displayName;
+    @FXML
+    private Label displayDesc;
 }
