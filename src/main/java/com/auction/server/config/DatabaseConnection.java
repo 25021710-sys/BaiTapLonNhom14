749f9cc378776fe755934d7ecbc2a768a3992093
25021710-sys <25021710@vnu.edu.vn>
2026-04-23 12:05:31 +0700

    Upload

--- src/main/java/com/auction/server/config/DatabaseConnection.java
+++ src/main/java/com/auction/server/config/DatabaseConnection.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     // 1. Khai báo 1 biến duy nhất để kết nối
-    private static Connection connection = null;
+//    private static Connection connection = null;
+    private static volatile DatabaseConnection instane;
 
     // 2. Thông tin "địa chỉ" của Railway
     private static final String URL = "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/railway";
@@ -16,20 +17,33 @@ public class DatabaseConnection {
     // 3. Đặt hàm khởi tạo là 'private' -> Ngăn chặn người khác tạo thêm
     private DatabaseConnection() {
     }
-
-    // 4. Đây là cái "công tắc" duy nhất để mọi nơi trong code gọi đến khi cần dùng Database
-    public static Connection getConnection() {
-        try {
-            if (connection == null || connection.isClosed()) {
-                // Khai báo việc sử dụng "người phiên dịch" MySQL
-                Class.forName("com.mysql.cj.jdbc.Driver");
-
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                System.out.println("Đã kết nối thành công tới Database Railway!");
+    public static DatabaseConnection getInstance(){
+        if(instane==null){
+            synchronized (DatabaseConnection.class){
+                if (instane==null){
+                    instane=new DatabaseConnection();
+                }
             }
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Lỗi kết nối Database: " + e.getMessage());
         }
-        return connection;
+        return instane;
     }
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+    // 4. Đây là cái "công tắc" duy nhất để mọi nơi trong code gọi đến khi cần dùng Database
+//    public static Connection getConnection() {
+//        try {
+//            if (connection == null || connection.isClosed()) {
+//                // Khai báo việc sử dụng "người phiên dịch" MySQL
+//                Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+//                System.out.println("Đã kết nối thành công tới Database Railway!");
+//            }
+//        } catch (ClassNotFoundException | SQLException e) {
+//            System.out.println("Lỗi kết nối Database: " + e.getMessage());
+//        }
+//        return connection;
+//    }
+
 }
