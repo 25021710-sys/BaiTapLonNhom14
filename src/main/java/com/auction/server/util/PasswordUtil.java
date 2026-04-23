d877a6e000b0ddb9eb74d605a19f46ad9a6e4560
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-04-23 19:09:34 +0700

    User

--- src/main/java/com/auction/server/util/PasswordUtil.java
+++ src/main/java/com/auction/server/util/PasswordUtil.java
@@ -11,18 +11,18 @@ public class PasswordUtil {
     }
     public static String generationSalt(){
         SecureRandom random = new SecureRandom();
-        byte[] salt=new byte[16];
+        byte[] salt = new byte[16];
         random.nextBytes(salt);
         return Base64.getEncoder().encodeToString(salt);
     }
     public static String hash(String password, String salt){
-        try{
-            MessageDigest md=MessageDigest.getInstance("SHA-256");
-            String salted=salt+password;
-            byte[] hashed=md.digest(salted.getBytes(StandardCharsets.UTF_8));
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            String salted = salt + password;
+            byte[] hashed = md.digest(salted.getBytes(StandardCharsets.UTF_8));
             return Base64.getEncoder().encodeToString(hashed);
-        }catch (NoSuchAlgorithmException e){
-            throw new RuntimeException("SHA-256 không khả dụng",e);
+        } catch (NoSuchAlgorithmException e){
+            throw new RuntimeException("SHA-256 không khả dụng", e);
         }
     }
     public static boolean verify(String password,String salt,String storeHash){
