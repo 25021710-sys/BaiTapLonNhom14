d7c1980fe2eb229224a9a48a826bf17634d50eca
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-04-29 10:04:28 +0700

    sua phan user

--- src/main/java/com/auction/client/controller/BalanceController.java
+++ src/main/java/com/auction/client/controller/BalanceController.java
@@ -1,5 +1,10 @@
 package com.auction.client.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class BalanceController {
 
+    @FXML
+    private Label balanceLabel;
 }
