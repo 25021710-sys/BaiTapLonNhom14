553ac2778170e531480969f8008ac0f26c583142
thinhducdepzai-dev <thinhducdepzai@gmail.com>
2026-04-29 13:32:40 +0700

    bat dau lam phan current balance

--- src/main/java/com/auction/client/controller/BalanceController.java
+++ src/main/java/com/auction/client/controller/BalanceController.java
@@ -1,10 +1,29 @@
 package com.auction.client.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.TextFlow;
 
 public class BalanceController {
 
+    @FXML private TextField depositField;
+    @FXML private TextField withdrawField;
+    @FXML private Button ConfirmButton;
+    @FXML private TextFlow depositFlow;
+    @FXML private TextFlow withdrawFlow;
+    @FXML private Label balanceLabel;
     @FXML
-    private Label balanceLabel;
+    public void deposit(){
+
+    }
+    @FXML
+    public void withdraw(){
+
+    }
+    @FXML
+    public void handleConfirm(){
+
+    }
 }
